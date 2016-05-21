c4f1bf559d889987587679a2d9581f384e8dc7d3
Ethersmike <vganich@hotmail.com>
2016-05-21 13:58:42 +0300

    hwBasic3Addition

---
+++ vitya/src/main/java/hw/hwBasic3Addition/Main.java
@@ -0,0 +1,13 @@
+package hw.hwBasic3Addition;
+
+/**
+ * Created by User on 21.05.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+    ExtendClass extendClass=new ExtendClass();
+        extendClass.Print();
+        System.out.println((char) 27 + "[33m"+extendClass.color + (char)27 + "[0m");
+        System.out.println((char) 27 + "[36m"+extendClass.color + (char)27 + "[0m");
+    }
+}
---
+++ vitya/src/main/java/hw/hwBasic3Addition/Printer.java
@@ -0,0 +1,6 @@
+package hw.hwBasic3Addition;
+
+public class Printer {
+    public String color = "My color";
+    public String colorNext = "My color";
+}
