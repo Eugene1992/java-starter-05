e7b1dca85b1b7b49f21f4c688592180530319fe8
Ethersmike <vganich@hotmail.com>
2016-05-30 20:35:35 +0300

    hwBasic4Hw3

--- vitya/src/main/java/hw/hwBasic4Hw3/Main.java
+++ vitya/src/main/java/hw/hwBasic4Hw3/Main.java
@@ -3,6 +3,7 @@ package hw.hwBasic4Hw3;
 /**
  * Created by User on 30.05.2016.
  */
+//Обучился использыванию интерфейса)
 public class Main {
     public static void main(String[] args) {
         Player play = new Player();
