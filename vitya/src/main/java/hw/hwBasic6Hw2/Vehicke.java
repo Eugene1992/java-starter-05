b51a78df8757439cfd372ea0c92b250597bd5817
Ethersmike <vganich@hotmail.com>
2016-05-31 19:44:08 +0300

    hwBasic6Hw2

--- vitya/src/main/java/hw/hwBasic6Hw2/Vehicke.java
+++ vitya/src/main/java/hw/hwBasic6Hw2/Vehicke.java
@@ -7,4 +7,28 @@ package hw.hwBasic6Hw2;
 //которые также должны содержать метод print. Создайте экземпляры классов Wheel и Door.
 
 public class Vehicke {
+    void print() {
+        System.out.println("Vehicle");
+    }
+
+    public static class Wheel {
+        void print() {
+            System.out.println("Vehicle Too");
+        }
+    }
+
+    public static class Door {
+        void print() {
+            System.out.println("Vehicle Three");
+        }
+    }
+
+    public static void main(String[] args) {
+        Vehicke veh = new Vehicke();
+        veh.print();
+        Wheel veh2 = new Wheel();
+        veh2.print();
+        Door veh3 = new Door();
+        veh3.print();
+    }
 }
