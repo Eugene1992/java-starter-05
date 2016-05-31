f05eca7a89f3e4340c3d2b6e3758662e1a3cd1e5
Ethersmike <vganich@hotmail.com>
2016-05-31 10:30:44 +0300

    hwBasic6Addition+hw2

--- vitya/src/main/java/hw/hwBasic6Addition/Calculator.java
+++ vitya/src/main/java/hw/hwBasic6Addition/Calculator.java
@@ -36,20 +36,4 @@ public class Calculator {
         System.out.println("Ymnozenie");
         return a * b;
     }
-
-    public int getA() {
-        return a;
-    }
-
-    public void setA(int a) {
-        this.a = a;
-    }
-
-    public int getB() {
-        return b;
-    }
-
-    public void setB(int b) {
-        this.b = b;
-    }
 }
---
+++ vitya/src/main/java/hw/hwBasic6Hw2/Vehicke.java
@@ -0,0 +1,10 @@
+package hw.hwBasic6Hw2;
+
+/**
+ * Created by User on 31.05.2016.
+ */
+//Создать класс Vehicle с методом print. В классе Vehicle, создайте внутренний класс Wheel и Door
+//которые также должны содержать метод print. Создайте экземпляры классов Wheel и Door.
+
+public class Vehicke {
+}
