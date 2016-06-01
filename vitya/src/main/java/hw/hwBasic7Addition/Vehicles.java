c1f295c9e457748cb12bc37db201ff4070e7fed7
Ethersmike <vganich@hotmail.com>
2016-06-01 21:07:28 +0300

    hwBasic7Addition

---
+++ vitya/src/main/java/hw/hwBasic7Addition/Vehicles.java
@@ -0,0 +1,30 @@
+package hw.hwBasic7Addition;
+
+/**
+ * Created by User on 31.05.2016.
+ */
+
+//Создайте перечислительный тип (enum) Vehicles, содержащий конструктор, который должен принимать
+//целочисленное значение (стоимость автомобиля), содержать метод getColor(), который возвращает
+//строку с цветом автомобиля, и содержать перегруженный метод toString(), который должен возвращать
+//строку с названием экземпляра, цветом и  стоимостью автомобиля.
+
+public enum Vehicles {
+    ;
+    private int price;
+
+    Vehicles(int price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return "Green";
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicles{" +
+                "price=" + price + getColor() +
+                '}';
+    }
+}
